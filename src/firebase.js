349561b402832a78eb784ac1ea4d3f0c9396dca5
Shreyas Wani <wshreyas0@gmail.com>
2025-01-02 15:01:23 +0530

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.A_FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyADFXlLELKn-8iyINzZF2xSJyJwMDGvNi8",
+  authDomain: "chat-b2457.firebaseapp.com",
+  projectId: "chat-b2457",
+  storageBucket: "chat-b2457.appspot.com",
+  messagingSenderId: "108449723664",
+  appId: "1:108449723664:web:623e4d1141f1b37ca62195"
 };
 
 
