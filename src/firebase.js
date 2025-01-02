171f9eb031d60c1dfe393deb11a876a1e70a00a2
Shreyas Wani <wshreyas0@gmail.com>
2025-01-02 14:58:42 +0530

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.A_FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
